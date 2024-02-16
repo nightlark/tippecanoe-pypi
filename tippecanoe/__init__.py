202f933c1f2179f717ea52cf9ea6710e0d3a3c57
Ryan Mast <mast9@llnl.gov>
2024-02-16 12:51:36 -0800

    Fix pre-commit errors

--- tippecanoe/__init__.py
+++ tippecanoe/__init__.py
@@ -8,26 +8,32 @@ except ModuleNotFoundError:
     __version__ = ""
     __version_tuple__ = ()
 
-BIN_DIR = os.path.join(os.path.dirname(__file__), 'bin')
+BIN_DIR = os.path.join(os.path.dirname(__file__), "bin")
+
 
 def _program(name: str, *args):
     return subprocess.call([os.path.join(BIN_DIR, name)] + list(args))
 
+
 def tippecanoe():
-    raise SystemExit(_program('tippecanoe', *sys.argv[1:]))
+    raise SystemExit(_program("tippecanoe", *sys.argv[1:]))
+
 
 def tile_join():
-    raise SystemExit(_program('tile-join', *sys.argv[1:]))
+    raise SystemExit(_program("tile-join", *sys.argv[1:]))
+
 
 def tippecanoe_decode():
-    raise SystemExit(_program('tippecanoe-decode', *sys.argv[1:]))
+    raise SystemExit(_program("tippecanoe-decode", *sys.argv[1:]))
+
 
 def tippecanoe_enumerate():
-    raise SystemExit(_program('tippecanoe-enumerate', *sys.argv[1:]))
+    raise SystemExit(_program("tippecanoe-enumerate", *sys.argv[1:]))
+
 
 def tippecanoe_json_tool():
-    raise SystemExit(_program('tippecanoe-json-tool', *sys.argv[1:]))
+    raise SystemExit(_program("tippecanoe-json-tool", *sys.argv[1:]))
+
 
 def tippecanoe_overzoom():
-    raise SystemExit(_program('tippecanoe-overzoom', *sys.argv[1:]))
-
+    raise SystemExit(_program("tippecanoe-overzoom", *sys.argv[1:]))
