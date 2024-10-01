fd807b6daf79080277ef3faae7fd3e31501ad216
Mast <mast9@llnl.gov>
2024-10-01 16:33:33 -0700

    Mark code for open source release

--- tippecanoe/__init__.py
+++ tippecanoe/__init__.py
@@ -1,3 +1,7 @@
+# Copyright 2024 Lawrence Livermore National Security, LLC and other Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 import os
 import subprocess
 import sys
